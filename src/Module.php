f55c2caac4b190b35e89900bb3596669917f467a
Claire Matthews <poeticdragon@stormblaze.net>
2019-10-03 10:37:22 -0700

    Moved src files to be PSR-4 compliant

--- src/Module.php
+++ src/Module.php
@@ -8,7 +8,7 @@ class Module
 {
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     public function getAutoloaderConfig()
@@ -16,7 +16,7 @@ class Module
         return [
             'Zend\Loader\StandardAutoloader' => [
                 'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__ . '/src',
                 ],
             ],
         ];
